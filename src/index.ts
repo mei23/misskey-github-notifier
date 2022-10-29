75d9ef5566e71088d5ca41fbeb9e56b1c13a0265
mei23 <m@m544.net>
2022-10-29 12:31:16 +0900

    イベントを使わない

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import * as http from 'http';
-import { EventEmitter } from 'events';
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as bodyParser from 'koa-bodyparser';
 import * as request from 'request';
 const crypto = require('crypto');
 const config = require('../config.json');
-
-const handler = new EventEmitter();
 
 const post = async (text: string, home = true) => {
 	request.post(config.instance + '/api/notes/create', {
@@ -29,17 +26,20 @@ const secret = config.hookSecret;
 const router = new Router();
 
 router.post('/github', ctx => {
-	const body = JSON.stringify(ctx.request.body);
+	const type = ctx.headers['x-github-event'];
+	const event = ctx.request.body;
+	const body = JSON.stringify(event);
 	const hash = crypto.createHmac('sha1', secret).update(body).digest('hex');
 	const sig1 = new Buffer(ctx.headers['x-hub-signature']);
 	const sig2 = new Buffer(`sha1=${hash}`);
 
 	// シグネチャ比較
 	if (sig1.equals(sig2)) {
-		handler.emit(ctx.headers['x-github-event'], ctx.request.body);
-		console.log(`GitHubHook: ${JSON.stringify(ctx.request.body)}`);
+		console.log(type, event);
+		handle(type, event);
 		ctx.status = 204;
 	} else {
+		console.log('400');
 		ctx.status = 400;
 	}
 });
@@ -52,113 +52,129 @@ server.requestTimeout = 60 * 1000;
 
 server.listen(config.port);
 
-handler.on('status', event => {
-	const state = event.state;
-	switch (state) {
-		case 'error':
-		case 'failure':
-			const commit = event.commit;
-			const parent = commit.parents[0];
-
-			// Fetch parent status
-			request({
-				url: `${parent.url}/statuses`,
-				proxy: config.proxy,
-				headers: {
-					'User-Agent': 'misskey'
-				}
-			}, (err, res, body) => {
-				if (err) {
-					console.error(err);
-					return;
-				}
-				const parentStatuses = JSON.parse(body);
-				const parentState = parentStatuses[0].state;
-				const stillFailed = parentState == 'failure' || parentState == 'error';
-				if (stillFailed) {
-					post(`⚠️BUILD STILL FAILED⚠️: ?[${commit.commit.message}](${commit.html_url})`);
-				} else {
-					post(`🚨BUILD FAILED🚨: →→→?[${commit.commit.message}](${commit.html_url})←←←`);
-				}
-			});
+async function handle(type: string, event: any) {
+	switch (type) {
+		case 'status': {
+			const state = event.state;
+			switch (state) {
+				case 'error':
+				case 'failure':
+					const commit = event.commit;
+					const parent = commit.parents[0];
+		
+					// Fetch parent status
+					request({
+						url: `${parent.url}/statuses`,
+						proxy: config.proxy,
+						headers: {
+							'User-Agent': 'misskey'
+						}
+					}, (err, res, body) => {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						const parentStatuses = JSON.parse(body);
+						const parentState = parentStatuses[0].state;
+						const stillFailed = parentState == 'failure' || parentState == 'error';
+						if (stillFailed) {
+							post(`⚠️BUILD STILL FAILED⚠️: ?[${commit.commit.message}](${commit.html_url})`);
+						} else {
+							post(`🚨BUILD FAILED🚨: →→→?[${commit.commit.message}](${commit.html_url})←←←`);
+						}
+					});
+					break;
+			}
 			break;
-	}
-});
+		}
 
-handler.on('push', event => {
-	const ref = event.ref;
-	switch (ref) {
-		case 'refs/heads/mei-m544':
-			const pusher = event.pusher;
-			const compare = event.compare;
-			const commits: any[] = event.commits;
-			post([
-				`🆕 Pushed by ${pusher.name} with ?[${commits.length} commit${commits.length > 1 ? 's' : ''}](${compare}):`,
-				commits.reverse().map(commit => `・[?[${commit.id.substr(0, 7)}](${commit.url})] ${commit.message.split('\n')[0]}`).join('\n'),
-			].join('\n'));
+		case 'push': {
+			const ref = event.ref;
+			switch (ref) {
+				case 'refs/heads/mei-m544':
+					const pusher = event.pusher;
+					const compare = event.compare;
+					const commits: any[] = event.commits;
+					post([
+						`🆕 Pushed by ${pusher.name} with ?[${commits.length} commit${commits.length > 1 ? 's' : ''}](${compare}):`,
+						commits.reverse().map(commit => `・[?[${commit.id.substr(0, 7)}](${commit.url})] ${commit.message.split('\n')[0]}`).join('\n'),
+					].join('\n'));
+					break;
+			}
 			break;
-	}
-});
+		}
 
-handler.on('issues', event => {
-	const issue = event.issue;
-	const action = event.action;
-	let title: string;
-	switch (action) {
-		case 'opened': title = '💥 Issue opened'; break;
-		case 'closed': title = '💮 Issue closed'; break;
-		case 'reopened': title = '🔥 Issue reopened'; break;
-		default: return;
-	}
-	post(`${title}: <${issue.number}>「${issue.title}」\n${issue.html_url}`);
-});
-
-handler.on('issue_comment', event => {
-	const issue = event.issue;
-	const comment = event.comment;
-	const action = event.action;
-	let text: string;
-	switch (action) {
-		case 'created': text = `💬 Commented to「${issue.title}」:${comment.user.login}「${comment.body}」\n${comment.html_url}`; break;
-		default: return;
-	}
-	post(text);
-});
-
-handler.on('release', event => {
-	const action = event.action;
-	const release = event.release;
-	let text: string;
-	switch (action) {
-		case 'published': text = `🎁 NEW RELEASE: [${release.tag_name}](${release.html_url}) is out now. Enjoy!`; break;
-		default: return;
-	}
-	post(text, false);
-});
-
-handler.on('watch', event => {
-	const sender = event.sender;
-	post(`⭐️ Starred by ${sender.login} ⭐️`, false);
-});
-
-handler.on('fork', event => {
-	const repo = event.forkee;
-	post(`🍴 Forked:\n${repo.html_url} 🍴`);
-});
-
-handler.on('pull_request', event => {
-	const pr = event.pull_request;
-	const action = event.action;
-	let text: string;
-	switch (action) {
-		case 'opened': text = `📦 New Pull Request:「${pr.title}」\n${pr.html_url}`; break;
-		case 'reopened': text = `🗿 Pull Request Reopened:「${pr.title}」\n${pr.html_url}`; break;
-		case 'closed':
-			text = pr.merged
-				? `💯 Pull Request Merged!:「${pr.title}」\n${pr.html_url}`
-				: `🚫 Pull Request Closed:「${pr.title}」\n${pr.html_url}`;
+		case 'issues': {
+			const issue = event.issue;
+			const action = event.action;
+			let title: string;
+			switch (action) {
+				case 'opened': title = '💥 Issue opened'; break;
+				case 'closed': title = '💮 Issue closed'; break;
+				case 'reopened': title = '🔥 Issue reopened'; break;
+				default: return;
+			}
+			post(`${title}: <${issue.number}>「${issue.title}」\n${issue.html_url}`);
 			break;
-		default: return;
+		}
+
+		case 'issue_comment': {
+			const issue = event.issue;
+			const comment = event.comment;
+			const action = event.action;
+			let text: string;
+			switch (action) {
+				case 'created': text = `💬 Commented to「${issue.title}」:${comment.user.login}「${comment.body}」\n${comment.html_url}`; break;
+				default: return;
+			}
+			post(text);
+			break;
+		}
+		
+		case 'release': {
+			const action = event.action;
+			const release = event.release;
+			let text: string;
+			switch (action) {
+				case 'published': text = `🎁 NEW RELEASE: [${release.tag_name}](${release.html_url}) is out now. Enjoy!`; break;
+				default: return;
+			}
+			post(text, false);
+			break;
+		}
+		
+		case 'watch': {
+			const sender = event.sender;
+			post(`⭐️ Starred by ${sender.login} ⭐️`, false);
+			break;
+		}
+		
+		case 'fork': {
+			const repo = event.forkee;
+			post(`🍴 Forked:\n${repo.html_url} 🍴`);
+			break;
+		}
+		
+		case 'pull_request': {
+			const pr = event.pull_request;
+			const action = event.action;
+			let text: string;
+			switch (action) {
+				case 'opened': text = `📦 New Pull Request:「${pr.title}」\n${pr.html_url}`; break;
+				case 'reopened': text = `🗿 Pull Request Reopened:「${pr.title}」\n${pr.html_url}`; break;
+				case 'closed':
+					text = pr.merged
+						? `💯 Pull Request Merged!:「${pr.title}」\n${pr.html_url}`
+						: `🚫 Pull Request Closed:「${pr.title}」\n${pr.html_url}`;
+					break;
+				default: return;
+			}
+			post(text);
+			break;
+		}
+
+		default: {
+			console.log(`skip: ${type}`);
+		}
 	}
-	post(text);
-});
+}
